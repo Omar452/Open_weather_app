6d8b41584a4afffdbe37947525d5713582e0ad13
Omar <toumiomar452@gmail.com>
2021-03-15 12:47:54 +0000

    Create DOM element variables and apiKey variable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+let apiKey = 'e1084e724cab6801907df8de79c5c223';
+
+//elements from DOM
+let form = document.querySelector('form');
+let cityInput = document.querySelector('input:text');
+let weatherDiv = document.querySelector('#weather'); 
